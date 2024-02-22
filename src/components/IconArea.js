73301dc60164b4e7c774d574913175f15552ea27
asuf29 <asuf29@icloud.com>
2024-02-22 18:39:30 +0300

    created IconArea component

---
+++ src/components/IconArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+function IconArea() {
+  return (
+    <View>
+      <Image source={require('./../assets/images/logo.png')} />
+    </View>
+  );
+}
+
+export default IconArea;
