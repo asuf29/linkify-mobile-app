97ea7cc6a31f2fb3da38f659eaecd3057482dbeb
asuf29 <asuf29@icloud.com>
2024-03-12 15:13:03 +0300

    main design completed

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet, Button } from 'react-native';
 import Navbar from './../components/Navbar';
+import tw from 'twrnc';
 
 function ProfileScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Navbar />
-      <Text>Profile Screen</Text>
+      <View style={tw`flex-row items-center mb-20`}>
+        <View>
+          <Image
+            source={require('./../assets/images/asuf.jpg')}
+            style={tw`w-24 h-24  rounded-full`}
+          />
+        </View>
+        <View style={tw`flex-row mx-8`}>
+          <View style={tw`items-center mr-10`}>
+            <Text style={tw`text-lg font-bold`}>129</Text>
+            <Text style={tw`text-sm font-medium`}>followers</Text>
+          </View>
+          <View style={tw`items-center`}>
+            <Text style={tw`text-lg font-bold`}>129</Text>
+            <Text style={tw`text-sm font-medium`}>following</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bioContainer}>
+        <Text>Asuf</Text>
+      </View>
+      <View style={tw`flex-row`}>
+        <Button title="Edit Profile" onPress={() => {}} />
+        <Button title="Share profile" onPress={() => {}} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bioContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+});
 
 export default ProfileScreen;
