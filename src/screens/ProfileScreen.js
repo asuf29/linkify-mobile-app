17ed4a18e2e930c91c70cac8b4df05d00e266801
rizadogukankosar <rizadogukankosar@gmail.com>
2024-03-13 15:58:36 +0300

    log out button activated

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import { Text, View, Image, StyleSheet, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Navbar from './../components/Navbar';
 import tw from 'twrnc';
+import LoginScreen from './LoginScreen';
 
 function Feed() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
+    <View style={styles.container}>
+      <Navbar />
+      <View style={tw`flex-row items-center mb-20`}>
+        <View>
+          <Image
+            source={require('./../assets/images/asuf.jpg')}
+            style={tw`w-24 h-24  rounded-full`}
+          />
+        </View>
+        <View style={tw`flex-row mx-8`}>
+          <View style={tw`items-center mr-10`}>
+            <Text style={tw`text-lg font-bold`}>129</Text>
+            <Text style={tw`text-sm font-medium`}>followers</Text>
+          </View>
+          <View style={tw`items-center`}>
+            <Text style={tw`text-lg font-bold`}>129</Text>
+            <Text style={tw`text-sm font-medium`}>following</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bioContainer}>
+        <Text>Asuf</Text>
+      </View>
+      <View style={tw`flex-row`}>
+        <Button title="Edit Profile" onPress={() => {}} />
+        <Button title="Share profile" onPress={() => {}} />
+      </View>
     </View>
   );
 }
 
-function Article() {
+
+function LogOut({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Article Screen</Text>
+      <Text>Log out of your account</Text>
+      <Button
+        title="Go to LoginScreen"
+        onPress={() => navigation.navigate('LoginScreen')}
+      />
     </View>
   );
 }
@@ -25,45 +55,20 @@ const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
+    <Drawer.Navigator
+      screenOptions={{drawerPosition:"right"}}
+      // screenOptions={{headerShown: false}}
+    >
+      <Drawer.Screen name="Profile" component={Feed} />
+      <Drawer.Screen name="Log Out" component={LogOut} />
     </Drawer.Navigator>
   );
 }
 
 function ProfileScreen() {
-  return <MyDrawer />;
-  //return (
-  //   <View style={styles.container}>
-  //     <Navbar />
-  //     <View style={tw`flex-row items-center mb-20`}>
-  //       <View>
-  //         <Image
-  //           source={require('./../assets/images/asuf.jpg')}
-  //           style={tw`w-24 h-24  rounded-full`}
-  //         />
-  //       </View>
-  //       <View style={tw`flex-row mx-8`}>
-  //         <View style={tw`items-center mr-10`}>
-  //           <Text style={tw`text-lg font-bold`}>129</Text>
-  //           <Text style={tw`text-sm font-medium`}>followers</Text>
-  //         </View>
-  //         <View style={tw`items-center`}>
-  //           <Text style={tw`text-lg font-bold`}>129</Text>
-  //           <Text style={tw`text-sm font-medium`}>following</Text>
-  //         </View>
-  //       </View>
-  //     </View>
-  //     <View style={styles.bioContainer}>
-  //       <Text>Asuf</Text>
-  //     </View>
-  //     <View style={tw`flex-row`}>
-  //       <Button title="Edit Profile" onPress={() => {}} />
-  //       <Button title="Share profile" onPress={() => {}} />
-  //     </View>
-  //   </View>
-  // );
+  return (
+    <MyDrawer />
+  );
 }
 
 const styles = StyleSheet.create({
