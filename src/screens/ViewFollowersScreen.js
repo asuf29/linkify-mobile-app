9d856c35107c98eb7a9ee9c45865cbca21492cea
rizadogukankosar <rizadogukankosar@gmail.com>
2024-05-26 19:54:42 +0300

    added view followers and following

---
+++ src/screens/ViewFollowersScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import tw from 'twrnc';
+
+function ViewFollowersScreen() {
+  return (
+    <View style={tw`flex-1 items-center justify-center`}>
+      <Text>View Followers Screen</Text>
+    </View>
+  );
+}
+
+export default ViewFollowersScreen;
---
+++ src/screens/ViewFollowingScreen.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import tw from 'twrnc';
+
+function ViewFollowingScreen() {
+  return (
+    <View style={tw`flex-1 items-center justify-center`}>
+      <Text>View Following Screen</Text>
+    </View>
+  );
+}
+
+export default ViewFollowingScreen;
+
