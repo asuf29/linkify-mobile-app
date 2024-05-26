1fe660e9b09b6955e13c18196563a97bd3946e30
rizadogukankosar <rizadogukankosar@gmail.com>
2024-05-26 21:43:56 +0300

    add category list

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
 import Navbar from './../components/Navbar';
 import { RefreshControl, ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import UserSearchBar from './UserSearchBar';
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const iconUrl = (value) => {
+  console.log("value", value)
+  switch (value) {
+    case 1:
+      return require("../assets/images/icons/1.png");
+    case 2:
+      return require("../assets/images/icons/2.png");
+    case 3:
+      return require("../assets/images/icons/3.png");
+    case 4:
+      return require("../assets/images/icons/4.png");
+    case 5:
+      return require("../assets/images/icons/5.png");
+    case 6:
+      return require("../assets/images/icons/6.png");
+    case 7:
+      return require("../assets/images/icons/7.png");
+    case 8:
+      return require("../assets/images/icons/8.png");
+    default:
+      return require("../assets/images/icons/1.png");
+  }
+};
 
 function DiscoverScreen() {
   const navigation = useNavigation();
   const [refreshing, setRefreshing] = React.useState(false);
+  const [categoryList, setCategoryList] = useState([]);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -17,6 +44,36 @@ function DiscoverScreen() {
       setRefreshing(false);
     }, 2000);
   }, []);
+
+  useEffect(() => {
+    const handleCategoryList = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+        const response = await axios.get('https://linkify-backend-test-94b3648c3afa.herokuapp.com/api/posts/category_list', {
+          headers: {
+            'Authorization': `${token}`
+          }
+        });
+        const { data, code } = response.data;
+        if (code === 200) {
+          console.log(data);
+          setCategoryList(data);
+          console.log("aa")
+        } else {
+          console.log(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleCategoryList();
+  }, []);
+
+  const renderCategoryItem = ({ item }) => (
+    <TouchableOpacity style={styles.categoryItem}>
+      <Image source={iconUrl(item.id)} style={styles.icon} />
+    </TouchableOpacity>
+  );
   
   return (
     <SafeAreaView style={styles.container}>
@@ -33,6 +90,13 @@ function DiscoverScreen() {
             </TouchableOpacity>
           </View>
           <Text>Discover Screen</Text>
+          <FlatList
+            data={categoryList}
+            renderItem={renderCategoryItem}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            contentContainerStyle={styles.categoryList}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -65,6 +129,27 @@ const styles = StyleSheet.create({
   },
   searchIcon: {
     marginLeft: 10,
+  },
+  categoryList: {
+    paddingVertical: 10,
+  },
+  categoryItem: {
+    backgroundColor: '#f0f0f0',
+    borderRadius: 50,
+    padding: 15,
+    marginHorizontal: 5,
+    alignItems: 'center',
+    borderBlockColor: 'black',
+    borderWidth: 1,
+    height: 100,
+    width: 100,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    marginHorizontal: 5,
+    marginTop: 5,
+    alignItems: 'center',
   },
 });
 
